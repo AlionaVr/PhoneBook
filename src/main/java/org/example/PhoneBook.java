d9ea2c309dae5ce534cfa2ba415214fb9166141e
AlionaVr <alonavershilo@yandex.by>
2025-03-15 19:50:27 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,10 +1,17 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.TreeMap;
+
 public class PhoneBook {
+    private final TreeMap<String, String> nameToNumber = new TreeMap<>();
+    private final HashMap<String, String> numberToName = new HashMap<>();
+
     public int add(String name, String number) {
-        // добавить имя с номером.
-        // Метод должен возвращать количество контактов после добавления,
-        // при этом гарантируется,что не будут добавляться повторяющиеся имена;
-        return 0;
+        if (!nameToNumber.containsKey(name)) {
+            nameToNumber.put(name, number);
+            numberToName.put(number, name);
+        }
+        return nameToNumber.size();
     }
 }
