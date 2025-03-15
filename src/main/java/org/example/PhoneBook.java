88c194ce722f351749da3be4b72c0ca039e83cfe
AlionaVr <alonavershilo@yandex.by>
2025-03-15 19:37:01 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class PhoneBook {
+    public int add(String name, String number) {
+        // добавить имя с номером.
+        // Метод должен возвращать количество контактов после добавления,
+        // при этом гарантируется,что не будут добавляться повторяющиеся имена;
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,30 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+
+    @Test
+    void whenAddContactReturnCorrectCountOfContacts() {
+        //arrange
+        PhoneBook phoneBook = new PhoneBook();
+        int expectedContactCount = 1;
+        //act
+        int actualContactCount = phoneBook.add("Aliona", "+375(33)862-31-55");
+        //assert
+        assertEquals(expectedContactCount, actualContactCount);
+    }
+
+    @Test
+    void whenAddDuplicatedContactReturnUnchangedCountOfContacts() {
+        //arrange
+        PhoneBook phoneBook = new PhoneBook();
+        int expectedContactCount = 1;
+        //act
+        phoneBook.add("Aliona", "+375(33)862-31-55");
+        int actualContactCount = phoneBook.add("Aliona", "+375(33)862-31-55");
+        //assert
+        assertEquals(expectedContactCount, actualContactCount);
+    }
 }
